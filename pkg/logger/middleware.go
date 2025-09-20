96214fc1b11ca7bd3a5d8b8c616007d027de6178
Saveliy Tretyakov <tretyakov.sava@gmail.com>
2025-09-19 18:41:34 -0700

    added logging middleware

---
+++ pkg/logger/middleware.go
@@ -0,0 +1,24 @@
+package logger
+
+import (
+	"context"
+	"net/http"
+)
+
+type Middleware func(next http.Handler) http.Handler
+
+func NewLoggingMiddleware(lctx context.Context) (Middleware, error) {
+	log, err := LoggerFromCtx(lctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := context.WithValue(r.Context(), CtxKey, log)
+			r = r.WithContext(ctx)
+
+			next.ServeHTTP(w, r)
+		})
+	}, nil
+}
