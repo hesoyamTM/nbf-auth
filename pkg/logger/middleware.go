7afd7886e36bbff297092424242ed958e0693969
Saveliy Tretyakov <tretyakov.sava@gmail.com>
2025-10-26 15:42:49 +0300

    added more fields to logging middleware

--- pkg/logger/middleware.go
+++ pkg/logger/middleware.go
@@ -3,6 +3,8 @@ package logger
 import (
 	"context"
 	"net/http"
+
+	"go.uber.org/zap"
 )
 
 type Middleware func(next http.Handler) http.Handler
@@ -15,6 +17,11 @@ func NewLoggingMiddleware(lctx context.Context) (Middleware, error) {
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log = log.With(
+				zap.String("remote_addr", r.RemoteAddr),
+				zap.String("method", r.Method),
+				zap.String("uri", r.RequestURI),
+			)
 			ctx := context.WithValue(r.Context(), CtxKey, log)
 			r = r.WithContext(ctx)
 
